4c44fb214154797785cddfed93b031cd8ba10264
Khdbble <fb503d52.af1c4d@gmail.com>
2021-05-31 19:20:33 +0900

    Initial Commit

---
+++ seatChange/seatChangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  seatChangeApp.swift
+//  seatChange
+//
+//  Created by naoki on 2021/05/31.
+//
+
+import SwiftUI
+
+@main
+struct seatChangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
